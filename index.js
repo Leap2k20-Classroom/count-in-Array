4ebfe3d958b61926658f46382a7f950a4bed8ed8
lhagvadarjaa <lhagva98@gmail.com>
2021-01-25 12:53:29 +0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+// Fix this function
+function count(arg) {
+   var arr  = arg.split(',');
+   //some write code;
+   
+   //return max repeated element
+}
+
+// Do not touch this
+console.log(count(process.argv[2]))
+
